c7fbb9062c3d287c98ab50a73750eb51eff1f668
akshayyelvankar <akshayyelvankar@gmail.com>
2023-05-09 16:54:01 +0530

    Adding Routing

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,7 +2,14 @@ import { useEffect } from 'react';
 import {fetchDataFromApi} from "./utils/api";
 import {useSelector,useDispatch} from 'react-redux';
 import { getApiConfiguration } from './Store/homeReducer';
-
+import {Route,Routes} from 'react-router-dom';
+import Header from "./Components/header/Header";
+import Footer from "./Components/footer/Footer";
+import Home from "./Pages/home/Home";
+import Explore from "./Pages/explore/Explore";
+import PageNotFound from "./Pages/404/PageNotFound";
+import SearchResult from "./Pages/searchResult/SearchResult";
+import Details from "./Pages/details/Details";
 function App() {
   
   const dispatch=useDispatch()
@@ -21,8 +28,15 @@ function App() {
   }
   return (
     <div className="App">
-      
-      
+         <Header/>
+         <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/:mediaType/:id' element={<Details/>}/>
+          <Route path='/search/:query' element={<SearchResult/>}/>
+          <Route path='/explore/:mediaType' element={<Explore/>}/>
+          <Route path='*' element={<PageNotFound/>}/>
+         </Routes>
+         <Footer/>
     </div>
   );
 }
