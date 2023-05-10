4e4f6521c5297eb4d39687978f8e21b4de743c84
akshayyelvankar <akshayyelvankar@gmail.com>
2023-05-10 15:47:31 +0530

    Today working on HeroBanner component & also working on functionality of fetching random background images

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -10,19 +10,27 @@ import Explore from "./Pages/explore/Explore";
 import PageNotFound from "./Pages/404/PageNotFound";
 import SearchResult from "./Pages/searchResult/SearchResult";
 import Details from "./Pages/details/Details";
+
 function App() {
   
   const dispatch=useDispatch()
   const {url}=useSelector((state)=>state.home)
   console.log(url)
   useEffect(()=>{
-     apiTest()
+     fetchApiConfig()
   },[])
-  const apiTest=()=>{
-    fetchDataFromApi("/movie/popular")
+
+  const fetchApiConfig=()=>{
+    fetchDataFromApi("/configuration")
     .then((res)=>{
        console.log(res)
-       dispatch(getApiConfiguration(res))
+
+       const url={
+        backdrop:res.images.secure_base_url + "original",
+        poster:res.images.secure_base_url + "original",
+        profile:res.images.secure_base_url + "original",
+       }
+       dispatch(getApiConfiguration(url))
     })
     
   }
--- movie-app/src/Pages/home/heroBanner/HeroBanner.jsx
+++ movie-app/src/Pages/home/heroBanner/HeroBanner.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.scss';
+import {useNavigate} from "react-router-dom";
+import useFetch from '../../../hooks/useFetch';
+import Img from "../../../Components/lazyLoadImages/Img";
+import ContentWrapper from "../../../Components/contentWrapper/ContentWrapper";
+import { useSelector } from 'react-redux';
 const HeroBanner = () => {
+  const[background,setBackground]=useState("");
+  const[query,setQuery]=useState("");
+  const navigate=useNavigate();
+  const {url} =useSelector((state)=>state.home)
+  // Fetching Data From APi
+  const {data,loading}=useFetch("/movie/upcoming");
+
+  //Fetching Random Background image
+     useEffect(()=>{
+        const bg=url.backdrop + data?.results?.[Math.floor(Math.random()*20)]?.backdrop_path;
+         setBackground(bg);
+        },[data])
+   
+  const searchqueryHandler=(e)=>{
+       if(e.key ==="Enter" && query.length>0)
+       {
+            navigate(`/search/${query}`)
+       }
+  }
   return (
-    <div>
-       HeroBanner
+    <div className='heroBanner'>
+      {
+        !loading && <div className='backdrop-img'>
+            <Img src={background}/>
+         </div>
+      }
+        <div className='opacity-layer'></div>
+      <ContentWrapper>
+         <div className='heroBannerConent'>
+          <span className='title' style={{color:"#fff"}}>Welcome</span>
+          <span className='subTitle'>Millions of movies,TV shows and People to discover,Explore now.</span>
+          <div className='searchInput'>
+            <input type='text' placeholder='Search for a movie or tv show...' onKeyUp={searchqueryHandler} onChange={(e)=>setQuery(e.target.value)}/>
+            <button>Search</button>
+          </div>
+        </div>
+     </ContentWrapper>
+       
     </div>
   )
 }
